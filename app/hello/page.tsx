06b98edc96e6867f996e3c4336ffc4badb9b9c20
dooomon <dooo@nifty.com>
2024-06-26 01:29:49 +0900

    initial commit

---
+++ app/hello/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function HelloPage() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+          <h1 className="text-6xl font-bold">
+            Hello World
+          </h1>
+        </main>
+      </div>
+    )
+  }
+  
+  
